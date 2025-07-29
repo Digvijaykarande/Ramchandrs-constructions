9cc9bedd5924c45d436717436fe211b4dff7c74e
digvijaykarande <karandedigvijay2@gmail.com>
2025-07-29 21:25:39 +0530

    routing errors fixed

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import HomePage from './pages/HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <>
-    <HomePage />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
