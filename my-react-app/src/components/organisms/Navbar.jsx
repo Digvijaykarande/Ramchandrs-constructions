c0b21efc3fce9c1ab4ae1cab1a81dcaa62b2a7d6
digvijaykarande <karandedigvijay2@gmail.com>
2025-08-24 17:40:53 +0530

    fix responsiveness

--- my-react-app/src/components/organisms/Navbar.jsx
+++ my-react-app/src/components/organisms/Navbar.jsx
@@ -23,29 +23,6 @@ function Navbar() {
         </li>
       </ul>
 
-      {/* Mobile Toggle Button */}
-      <button 
-        className="navbar__toggle" 
-        onClick={() => setIsOpen(!isOpen)}
-        aria-label="Toggle menu"
-      >
-        {isOpen ? <X size={24} /> : <Menu size={24} />}
-      </button>
-
-      {/* Mobile Menu */}
-      <div className={`navbar__mobile ${isOpen ? "open" : ""}`}>
-        <ul>
-          <li><a href="/" onClick={() => setIsOpen(false)}>Home</a></li>
-          <li><a href="/products" onClick={() => setIsOpen(false)}>Products</a></li>
-          <li><a href="/categories" onClick={() => setIsOpen(false)}>Categories</a></li>
-          <li><a href="/about" onClick={() => setIsOpen(false)}>About</a></li>
-          <li>
-            <a href="/cart" className="navbar__cart" onClick={() => setIsOpen(false)}>
-              <ShoppingCart size={18} /> Cart
-            </a>
-          </li>
-        </ul>
-      </div>
     </nav>
   );
 }
